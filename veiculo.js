57a9b47a3a5e89c5444682fa6519bb34604717c5
igor de sousa <igorde1999sousa@gmail.com>
2024-06-22 11:47:25 -0300

    exercicio oo_js

---
+++ veiculo.js
@@ -0,0 +1,63 @@
+function Motorista(nome) {
+    this.nome = nome;
+    this.dizNome = function() {
+        console.log(this.nome);
+    }
+    this.dizAuto = function() {
+        console.log(this.auto);
+    }
+    this.dizAluguel = function() {
+        console.log(this.aluguel);
+    }
+}
+
+function Locacao(nome, auto, aluguel) {
+    let _aluguel = aluguel;
+
+    this.getAluguel = function() {
+        return _aluguel;
+    }
+
+    this.setAluguel = function(valor) { 
+    if (typeof valor === 'number') {
+            _aluguel = valor;
+    }
+    } 
+
+    this.diaria = function() {
+        const novoAluguel = _aluguel * 1.1;
+        _aluguel = novoAluguel;
+    }
+
+    Motorista.call(this, nome);
+}
+
+function Moto(nome) {
+    Locacao.call(this, nome, "Moto", 300);
+
+    this.diaria = function() {
+        const novoAluguel = this.getAluguel() * 1.07;
+        this.setAluguel(novoAluguel);
+    }
+}
+
+function Caminhao(nome) {
+    Locacao.call(this, nome, "Caminhao", 1000);
+    this.diaria = function() {
+        const novoAluguel = this.getAluguel() * 1.15; 
+        this.setAluguel(novoAluguel);
+    }
+}
+
+const cliente1 = new Locacao("Paula", "carro", 500);
+const cliente2 = new Moto("Pati");
+const cliente3 = new Caminhao("Mol");
+
+cliente1.diaria();
+console.log(cliente1.getAluguel());
+
+cliente2.diaria();
+console.log(cliente2.getAluguel());
+
+cliente3.diaria();
+console.log(cliente3.getAluguel());
